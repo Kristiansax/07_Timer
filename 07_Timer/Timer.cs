a23c6a987e9a2770d450828bc254ab8a4886cdbc
Kristiansax <Kristiansax@KRELLES_VISION>
2016-09-20 10:38:01 +0200

    Add project files.

---
+++ 07_Timer/Timer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace _07_Timer
+{
+    public class Timer
+    {
+        public int Minutes;
+        public int Hours;
+        private int seconds;
+        private string StringSeconds;
+        private string StringMinutes;
+        private string StringHours;
+        public int Seconds
+        {
+            set
+            {
+                seconds = value;
+            }
+            get
+            {
+                return seconds;
+            }
+        }
+        public override string ToString()
+        {
+            GetHoursMinutesSeconds();
+            AddZeroes();
+
+            return (StringHours + ":" + StringMinutes + ":" + StringSeconds);
+        }
+
+        private void GetHoursMinutesSeconds()
+        {
+            while (Seconds > 60)
+            {
+                //Seconds = Seconds - 60;
+                Minutes++;
+            }
+            while (Minutes > 60)
+            {
+                //Minutes = Minutes - 60;
+                Hours++;
+            }
+        }
+
+        private void AddZeroes()
+        {
+            if (Seconds < 10)
+                StringSeconds = Convert.ToString("0" + Seconds);
+            else
+                StringSeconds = Convert.ToString(Seconds);
+
+            if (Minutes < 10)
+                StringMinutes = Convert.ToString("0" + Minutes);
+            else
+                StringMinutes = Convert.ToString(Minutes);
+
+            if (Hours < 10)
+                StringHours = Convert.ToString("0" + Hours);
+            else
+                StringHours = Convert.ToString(Hours);
+        }
+    }
+}
